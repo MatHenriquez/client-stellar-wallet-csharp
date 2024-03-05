dd43600c7eaabd70854f506933e129bf789e242f
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-04 21:39:59 -0300

    Install and configure BlazorStrap

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using BlazorStrap;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using StellarWalletClient;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorStrap();
+
+await builder.Build().RunAsync();
